c7348e3083e7471cc040f6d9c77772434813b8f7
Hemen Estefaie <estefaie@gmail.com>
2019-10-20 22:55:18 +1100

    Add index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,74 @@
-const sum = (a: number, b: number) => a + b;
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { Question } from './types';
 
-export { sum };
+// define the Express app
+const app = express();
+
+// the database
+const questions: Array<Question> = [];
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+// retrieve all questions
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length
+  }));
+  res.send(qs);
+});
+
+// get a specific question
+app.get('/:id', (req, res) => {
+  const question = questions.filter(q => q.id === parseInt(req.params.id));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+// insert a new question
+app.post('/', (req, res) => {
+  const { title, description } = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: []
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post('/answer/:id', (req, res) => {
+  const { answer } = req.body;
+
+  const question = questions.filter(q => q.id === parseInt(req.params.id));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+
+  question[0].answers.push(answer);
+
+  res.status(200).send();
+});
+
+// start the server
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
